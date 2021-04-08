eddafc5b5f7e208dbe3bd120c6ee0a8bb25697bd
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-04-09 03:19:32 +0600

    Orders api created

--- app.js
+++ app.js
@@ -1,11 +1,68 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express()
+require('dotenv').config()
+// console.log(process.env.DB_USER);
+
+app.use(bodyParser.json());
+app.use(cors());
 const port = 5000
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrxvr.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("orders");
+
+  app.post('/addProduct', (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products)
+      .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount)
+      })
+  })
+
+  app.get('/products', (req, res) => {
+    const search = req.query.search;
+    productsCollection.find({name: {$regex: search}})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.get('/product/:key', (req, res) => {
+    productsCollection.find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection.find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+});
+
 
 app.get('/', (req, res) => {
   res.send('EMA JOHN V2 SERVER')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server Has Started at http://localhost:${port}`)
 })
